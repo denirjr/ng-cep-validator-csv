aa96794eb7f48b180e46e212dd828530a211e0c5
Denir <denir-junior19@live.com>
2022-06-10 13:21:31 -0300

    wip(): adjust upload modal rules

--- src/app/shared/components/csv-upload/csv-upload.component.ts
+++ src/app/shared/components/csv-upload/csv-upload.component.ts
@@ -12,9 +12,24 @@ export class CsvUploadComponent {
   @Output()
   public csvRecords: EventEmitter<CsvRecord> = new EventEmitter();
 
+  public isInvalidUpload = false;
+
   constructor(private csv: CsvService) {}
 
   public getCsv($event: Event) {
-    this.csv.read($event).subscribe((record) => this.csvRecords.emit(record));
+    this.csv.read($event).subscribe((record) => {
+      if (this.isInValid(record)) {
+        this.isInvalidUpload = true;
+      } else {
+        this.csvRecords.emit(record);
+      }
+    });
+  }
+
+  private isInValid(record: CsvRecord): boolean {
+    const serializedHeader = JSON.stringify(record?.headers);
+    const defaultHeader = '["Nome","CEP","Score"]';
+
+    return record?.rows?.length > 5 || serializedHeader !== defaultHeader;
   }
 }
